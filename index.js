151c99bd4145d81be5b7214795d77e86a7eaf3cb
casper <ycasperinho@gmail.com>
2018-09-18 23:24:32 +0300

    database storing

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // listen to the redis event
 const pubsub = require('./pubsub');
+const mysql = require('./mysql');
 
 pubsub.on('sell', function(data) {
     console.log('Sell ' + data.id + ' with amount equals to ' + data.amount);
 
+    var processedAmount = data.amount + 10;
+
+    mysql.query('update sells set node = ' + processedAmount + ' where id=' + data.id);
 
 });
